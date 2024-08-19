aa95f05de49dc294f6b9152cf7a7a589bdcfa8f5
Emanuel Finozzi <88401455+RaitoNashi@users.noreply.github.com>
2024-08-19 16:30:31 -0300

    Add files via upload
    Vista de cliente jurídico, físico, ingresar compra, entre otros.
    Controlador de cliente jurídico, físico, ingresar compra, entre otros.

---
+++ Versión0.1/Projecto_taller/src/Controlador/Ingresar_Compra_Controller/Ingresar_CompraController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controlador.Ingresar_Compra_Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author raito
+ */
+public class Ingresar_CompraController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
